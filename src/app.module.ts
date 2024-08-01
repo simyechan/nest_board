5bf877f902db73b3c38e38fb30bc7d5b5ac1338a
sim <simyechan87@gmail.com>
2024-08-01 10:34:12 +0900

    [수정] env 설정 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,39 @@
 import { Module } from '@nestjs/common';
 import { BoardModule } from './board/board.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeORMConfig } from './config/typeorm.config';
 import { CommentModule } from './comment/comment.module';
 import { ReplyModule } from './reply/reply.module';
 import { UserModule } from './user/user.module';
+import { User } from './user/board.user-entity';
+import { Boards } from './board/board.entity';
+import { Comment } from './comment/comment.entity';
+import { Reply } from './reply/reply.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(typeORMConfig),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('HOST'),
+        post: configService.get<number>('PORT'),
+        password: configService.get<string>('USERNAME'),
+        database: configService.get<string>('DATABASE'),
+        entities: [User, Comment, Boards, Reply],
+        // entities: [__dirname + '/../**/*.entity.{js,ts}'],
+        synchronize: true,
+        logging: true,
+      }),
+    }),
     BoardModule,
     CommentModule,
     ReplyModule,
-    UserModule]
+    UserModule,
+  ],
 })
 export class AppModule {}
