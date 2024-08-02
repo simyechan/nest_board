b4b937da06d54127437aeb62bed3614a3611af63
sim <simyechan87@gmail.com>
2024-08-02 18:38:27 +0900

    [수정] auth.module 수정

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,7 +4,6 @@ import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/user/board.user-entity';
 import { JwtModule } from '@nestjs/jwt';
-import * as config from 'config';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UserModule } from 'src/user/user.module';
 
